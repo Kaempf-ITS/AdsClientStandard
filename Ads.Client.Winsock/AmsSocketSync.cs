9b299c41ff54514a7a603ca0889d6119f3eb3143
Daniel Kämpf <develop@kaempf-its.de>
2020-07-13 17:29:25 +0200

    Projektdateien hinzufügen.

---
+++ Ads.Client.Winsock/AmsSocketSync.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ads.Client.Winsock
+{
+    public class AmsSocketSync : AmsSocketBaseSync
+    {
+        protected new AmsSocket amsSocket;
+        public AmsSocketSync(AmsSocket amsSocket) : base(amsSocket)
+        {
+            this.amsSocket = amsSocket;
+        }
+
+        protected override void Connect()
+        {
+            amsSocket.Socket.Bind(amsSocket.LocalEndPoint);
+            amsSocket.Socket.Connect(amsSocket.IpTarget, amsSocket.PortTarget);
+        }
+
+        public override void Send(byte[] message)
+        {
+            amsSocket.Socket.Send(message);
+        }
+
+        public override void Receive(byte[] message)
+        {
+            try
+            {
+                int totalread = 0;
+
+                while (totalread < message.Length)
+                {
+                    var read = amsSocket.Socket.Receive(message, totalread, message.Length - totalread, SocketFlags.None);
+                    totalread += read;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (!Object.ReferenceEquals(ex.GetType(), typeof(ObjectDisposedException))) throw;
+            }
+        }
+
+    }
+}
---
+++ Ads.Client/Helpers/AdsAttribute.cs
@@ -0,0 +1,68 @@
+﻿/*  Copyright (c) 2011 Inando
+ 
+    Permission is hereby granted, free of charge, to any person obtaining a 
+    copy of this software and associated documentation files (the "Software"), 
+    to deal in the Software without restriction, including without limitation 
+    the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+    and/or sell copies of the Software, and to permit persons to whom the 
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included 
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+ */
+
+using System;
+using System.Reflection;
+
+namespace Ads.Client
+{
+    /// <summary>
+    /// An attribute class for members of serializable classes.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class AdsAttribute : Attribute
+    {
+        /// <summary>
+        /// The byte size.
+        /// </summary>
+        public uint ByteSize;
+
+        /// <summary>
+        /// The order.
+        /// </summary>
+        public uint Order;
+
+        /// <summary>
+        /// The array size.
+        /// </summary>
+        public uint ArraySize;
+
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        public AdsAttribute()
+        {
+            ByteSize = 0;
+            Order = 99;
+            ArraySize = 1;
+        }
+
+		private MemberInfo member;
+        /// <summary>
+        /// Gets or sets the member.
+        /// </summary>
+        internal MemberInfo Member
+        {
+            get { return member; }
+            set { member = value; }
+        }
+    }
+}
